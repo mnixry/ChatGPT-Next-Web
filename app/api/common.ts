6273e8de6675edc61329a46fff02cc844ab9a407
Mix <32300164+mnixry@users.noreply.github.com>
2023-04-09 18:04:35 +0800

    add free token usage support

--- app/api/common.ts
+++ app/api/common.ts
@@ -2,8 +2,9 @@ import { NextRequest } from "next/server";
 
 const OPENAI_URL = "api.openai.com";
 const DEFAULT_PROTOCOL = "https";
-const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
-const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
+
+export const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
+export const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
 
 export async function requestOpenai(req: NextRequest) {
   const apiKey = req.headers.get("token");
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,87 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ACCESS_CODES } from "./app/api/access";
+import * as common from "./app/api/common";
+
 import md5 from "spark-md5";
 
 export const config = {
   matcher: ["/api/openai", "/api/chat-stream"],
 };
 
-export function middleware(req: NextRequest) {
+export class ObtainFreeTokenHelper {
+  static freeTokenProvider =
+    process.env.FREE_TOKEN_PROVIDER || "https://freeopenai.xyz/api.txt";
+  private tokenList: string[] = [];
+
+  async obtainTokens() {
+    const obtainedTokens: string[] = [];
+    const tokens = await fetch(ObtainFreeTokenHelper.freeTokenProvider)
+      .then((res) => res.text())
+      .catch(() => undefined);
+    if (!tokens) {
+      console.log("[TokenObtainer] failed to obtain free token");
+      return;
+    }
+    obtainedTokens.push(...tokens.split("\n").map((token) => token.trim()));
+    console.log("[TokenObtainer] set free token, amount: ", obtainedTokens.length);
+
+    await this.checkAndSetAllTokens(obtainedTokens);
+
+    if (this.tokenList.length === 0) {
+      console.log("[TokenObtainer] no available token");
+      return;
+    }
+    return this.tokenList[Math.floor(Math.random() * this.tokenList.length)];
+  }
+
+  private async checkAndSetAllTokens(newTokens: string[]) {
+    const tokenAvailabilityMap: Record<string, boolean> = {};
+    for (const token of newTokens) tokenAvailabilityMap[token] = false;
+    for (const token of this.tokenList) tokenAvailabilityMap[token] = false;
+    await Promise.all(
+      Object.keys(tokenAvailabilityMap).map(
+        async (token) =>
+          (tokenAvailabilityMap[token] = await this.checkTokenAvailability(
+            token,
+          ).catch(() => false)),
+      ),
+    );
+    this.tokenList = Object.keys(tokenAvailabilityMap).filter(
+      (token) => tokenAvailabilityMap[token],
+    );
+  }
+
+  private async checkTokenAvailability(token: string): Promise<boolean> {
+    const response = await fetch(
+      `${common.PROTOCOL}://${common.BASE_URL}/v1/completions`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          model: "text-ada-001",
+          prompt: "This is a test",
+          max_tokens: 5,
+        }),
+      },
+    ).then((res) => res.json());
+    const available =
+        response &&
+        response.hasOwnProperty("choices") &&
+        response.choices.length > 0,
+      truncatedToken = token.slice(0, 15) + "..." + token.slice(-10);
+    console.log(
+      `[TokenObtainer] token ${truncatedToken} is ${
+        available ? "available" : "not available"
+      }`,
+    );
+    return available;
+  }
+}
+
+export async function middleware(req: NextRequest) {
   const accessCode = req.headers.get("access-code");
   const token = req.headers.get("token");
   const hashedCode = md5.hash(accessCode ?? "").trim();
@@ -30,7 +105,9 @@ export function middleware(req: NextRequest) {
 
   // inject api key
   if (!token) {
-    const apiKey = process.env.OPENAI_API_KEY;
+    let apiKey =
+      process.env.OPENAI_API_KEY ||
+      (await new ObtainFreeTokenHelper().obtainTokens().catch(() => undefined));
     if (apiKey) {
       console.log("[Auth] set system token");
       req.headers.set("token", apiKey);
